7ff15e848aa4221cbedf994489dd63e47115e895
Jeff Mesnil <jmesnil@gmail.com>
2013-10-25 12:20:39 +0200

    move "GCDAsyncSocket.h" import to StompKit.m

--- StompKit/StompKit.h
+++ StompKit/StompKit.h
@@ -7,7 +7,6 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "GCDAsyncSocket.h"
 
 #pragma mark Frame headers
 
--- StompKit/StompKit.m
+++ StompKit/StompKit.m
@@ -7,6 +7,7 @@
 //
 
 #import "StompKit.h"
+#import "GCDAsyncSocket.h"
 
 #define kDefaultTimeout 5
 #define kVersion1_2 @"1.2"
