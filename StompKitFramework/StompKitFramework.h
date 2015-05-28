7d31384cd358e40a7bc4e70b7100016fc52265fc
dgoon <kslee@hpcnt.com>
2015-05-28 16:16:01 +0900

    StompKit 프레임웍 빌드

---
+++ StompKitFramework/StompKitFramework.h
@@ -0,0 +1,19 @@
+//
+//  StompKitFramework.h
+//  StompKitFramework
+//
+//  Created by dgoon on 2015. 5. 28..
+//  Copyright (c) 2015년 Jeff Mesnil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StompKitFramework.
+FOUNDATION_EXPORT double StompKitFrameworkVersionNumber;
+
+//! Project version string for StompKitFramework.
+FOUNDATION_EXPORT const unsigned char StompKitFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StompKitFramework/PublicHeader.h>
+
+
